c341d9816518630d5a1a9af120aeb57f5e855f0a
numba19 <zachlegesse@gmail.com>
2024-01-12 21:29:26 -0500

    Removed old code that was causing a fatal error

--- unscriber/unscriber/src/App.js
+++ unscriber/unscriber/src/App.js
@@ -1,26 +1,12 @@
 import Table from './Table';
 import './App.css';
 import data from './messages.json';
-import { GoogleLogin} from '@react-oauth/google';
 
 
 function App() {
 
   return (
     <div className="App">
-
-      {/* google one tap login button */}
-      
-      <GoogleLogin
-      onSuccess={credentialResponse => {
-        console.log(credentialResponse);
-      }}
-      onError={() => {
-        console.log('Login Failed');
-      }}
-      useOneTap
-      />;
-
       <header className="App-header">
         <Table data={data}/>
       </header>
